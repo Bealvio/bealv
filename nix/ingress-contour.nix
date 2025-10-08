8d52324a79fac9e13c16ae7885cb5ce1322ec888
Victor Hang <vhvictorhang@gmail.com>
2025-10-08 20:44:49 +0200

    feat ✨: enable gatewayapi contour
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- nix/ingress-contour.nix
+++ nix/ingress-contour.nix
@@ -17,7 +17,9 @@ pkgs.runCommand "ingress-contour"
     mkdir -p $out/
     kustomize init
     cp ${manifest01}/examples/contour/*.yaml ./
+    cp ${manifest01}/examples/gateway/00-crds.yaml ./
     kustomize edit add resource *.yaml
     cp ${manifest01}/examples/contour/*.yaml $out/
+    cp ${manifest01}/examples/gateway/00-crds.yaml $out/
     cp kustomization.yaml $out/
   ''
